cfd308c898f7eae0a4d2efd60729e07d1be37fba
Aniruddh Laharia <aniruddh.imp12@gmail.com>
2023-05-02 22:30:00 +0530

    first commit

---
+++ src/components/Remaining.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Remaining = () =>{
+    return(
+    <div className="alert alert-success">
+        <span>Remaining : 100Rs </span>
+    </div>
+    )
+}
+
+export default Remaining;
---
+++ src/components/Spent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spent = () =>{
+    return(
+        <div className="alert alert-primary">
+            <span> Spent So Far : 100Rs </span>
+            </div>
+        )
+}
+
+export default Spent;
